983c4abc997ff2df31703366ab447d7bd843ea0f
Parth Antala <52197140+parallel-appy@users.noreply.github.com>
2024-07-17 13:35:48 -0400

    Initial Commit

---
+++ AccessibilitySandbox/AccessibilitySandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessibilitySandboxApp.swift
+//  AccessibilitySandbox
+//
+//  Created by Parth Antala on 2024-07-17.
+//
+
+import SwiftUI
+
+@main
+struct AccessibilitySandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
